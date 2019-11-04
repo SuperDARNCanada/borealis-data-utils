92ea401cb02d8f00a8b7d9e14c61da95c7853347
dataman <dataman@gigadat.usask.ca>
2019-11-04 14:05:23 -0600

    only read file once

--- SNR/plot_antennas_iq_snr.py
+++ SNR/plot_antennas_iq_snr.py
@@ -9,6 +9,7 @@ This script is used to
 import matplotlib
 matplotlib.use('Agg')
 
+import copy
 import sys
 import os
 import math
@@ -23,8 +24,10 @@ from scipy.fftpack import fft
 from multiprocessing import Pool, Process
 
 from hdf5_rtplot_utils import plot_antennas_range_time
+from pydarn import BorealisRead
 
-iq_file_1 = '/data1/borealis_data/20190802/20190802.0000.02.sas.0.antennas_iq.hdf5'
+iq_file_1 = '/sddata/sas_borealis_data/antennas_iq/20190911/20190911.1600.02.sas.0.antennas_iq.hdf5'
+#iq_file_1 = '/data1/borealis_data/20190802/20190802.0000.02.sas.0.antennas_iq.hdf5'
 #iq_file_1 = '/data/borealis_data/20190710/20190710.1609.44.sas.0.output_ptrs_iq.hdf5'
 #glob.glob('/data/tempdat/detwiller/20190402.17*')
 
@@ -36,11 +39,14 @@ if __name__ == '__main__':
     
     arg_tuples = []
 
+    reader = BorealisRead(iq_file_1, 'antennas_iq', 'array')
+    arrays = reader.arrays
+
     for antenna_num in antenna_range:
-        arg_tuples.append((iq_file_1, 'array', antenna_num))
+        arg_tuples.append((iq_file_1, copy.deepcopy(arrays), antenna_num))
  
 #    arg_tuples.append((iq_file_1, 19))   
-    pool = Pool(processes=2)  # 8 worker processes
+    pool = Pool(processes=3)  # 8 worker processes
     pool.starmap(plot_antennas_range_time, arg_tuples)
         
         #p = Process(target=plot_output_ptrs_iq_file_power, args=(iq_file_1, antenna_num,))
