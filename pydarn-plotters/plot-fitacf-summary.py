50292f0519a3466802da4a3ba63c1e4169eded91
dataman <dataman@gigadat.usask.ca>
2019-12-17 15:02:08 -0600

    Change to SDarn for pydarn change

--- pydarn-plotters/plot-fitacf-summary.py
+++ pydarn-plotters/plot-fitacf-summary.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 
 fitacf_file = sys.argv[1]
 
-pydarn_reader = pydarn.DarnRead(fitacf_file)
+pydarn_reader = pydarn.SDarnRead(fitacf_file)
 fitacf_data = pydarn_reader.read_fitacf()
 
-
-pydarn.RTP.plot_summary(fitacf_data, figsize=(22, 17), beam_num=0, groundscatter=True)
-plt.savefig(fitacf_file + '.summaryplot.png', dpi=400)
+for beam in range(0,16):
+    pydarn.RTP.plot_summary(fitacf_data, figsize=(22, 17), beam_num=beam, groundscatter=True)
+    plt.savefig(fitacf_file + '.beam{}.summaryplot.png'.format(beam), dpi=400)
 #plt.show()
 
