19c5484939d7787c848a110d8e6de66b735b3989
mardet987 <marci.detwiller@usask.ca>
2020-03-12 15:58:04 -0600

    updating for pydarn updates

--- pydarn-plotters/plot_range_time_rawacf.py
+++ pydarn-plotters/plot_range_time_rawacf.py
@@ -7,18 +7,19 @@ import os
 plt.rcParams.update({'font.size': 28})
 
 rawacf_file = sys.argv[1]
-darn_read = pydarn.DarnRead(rawacf_file)
+darn_read = pydarn.SDarnRead(rawacf_file)
 rawacf_data = darn_read.read_rawacf()
 
 rawacf_file_basename = os.path.basename(rawacf_file)
 
 lognorm = colors.LogNorm
+#cbar = matplotlib.colorbar.make_axes()
 
 plt.figure(figsize=(32,16))
 
 pydarn.RTP.plot_range_time(rawacf_data, beam_num=0, parameter='pwr0',
-                          color_bar_label="pwr0", color_norm=lognorm,
-                          color_map='gnuplot2')
+                          colorbar_label="pwr0", norm=colors.LogNorm,
+                          cmap='jet', vmin=10, vmax=3e5) #my pref is gnuplot2)
 
 plt.title("Borealis " + rawacf_file_basename + " pwr0")
 plt.ylabel('Range Gates')
