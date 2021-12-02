636c5fdc964d2474ad322a2c0b88de1a072c67a5
RemingtonRohel <rar129@usask.ca>
2021-12-02 12:04:37 -0600

    Added more options to argparser.

--- SNR/plot_antennas_range_time.py
+++ SNR/plot_antennas_range_time.py
@@ -1,18 +1,14 @@
 # Copyright 2019 SuperDARN Canada, University of Saskatchewan
 # Author: Marci Detwiller
-
+# Modified: Dec. 2, 2021 (Remington Rohel)
 """
 This script is used to plot all antennas range-time data from a single
 antennas_iq file.
 """
 
 import argparse
-import copy
-import glob
-import os
-import sys
-
 from hdf5_rtplot_utils import plot_antennas_range_time
+
 
 def usage_msg():
     """
@@ -26,7 +22,7 @@ def usage_msg():
         The usage message on how to use this 
     """
 
-    usage_message = """ plot_antennas_range_time.py [-h] antennas_iq_file
+    usage_message = """ plot_antennas_range_time.py [-h] antennas_iq_file --antenna-nums --max-power --min-power --start-sample --end-sample
     
     Pass in the name of the antennas_iq array-restructured file that you want 
     to plot range time data from. Antenna numbers from 0 to 20 will be plotted 
@@ -39,14 +35,20 @@ def usage_msg():
 def plot_parser():
     parser = argparse.ArgumentParser(usage=usage_msg())
     parser.add_argument("antennas_iq_file", help="Name of the file to plot.")
+    parser.add_argument("--antenna-nums", help="Antenna indices to plot.")
+    parser.add_argument("--max-power", help="Maximum Power of color scale (dB).", default=40.0)
+    parser.add_argument("--min-power", help="Minimum Power of color scale (dB).", default=10.0)
+    parser.add_argument("--start-sample", help="Sample Number to start at.", default=0)
+    parser.add_argument("--end-sample", help="Sample Number to end at.", default=80)
     return parser
 
 
 if __name__ == '__main__':
-    parser = plot_parser()
-    args = parser.parse_args()
+    args_parser = plot_parser()
+    args = args_parser.parse_args()
 
     filename = args.antennas_iq_file
     
-    plot_antennas_range_time(filename) # plot all antennas
+    plot_antennas_range_time(filename, antenna_nums=args.antenna_nums, num_processes=3, vmax=args.max_power,
+                             vmin=args.min_power, start_sample=args.start_sample, end_sample=args.end_sample)
 
