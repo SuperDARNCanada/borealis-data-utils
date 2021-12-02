15faa80a622e29cb0e70db2fc9b8a09ebb1b4a0e
RemingtonRohel <rar129@usask.ca>
2021-12-02 13:17:40 -0600

    Added more options for plotting bfiq files.

--- SNR/plot_bfiq_range_time.py
+++ SNR/plot_bfiq_range_time.py
@@ -23,7 +23,7 @@ def usage_msg():
         The usage message on how to use this 
     """
 
-    usage_message = """ plot_bfiq_range_time.py [-h] bfiq_file
+    usage_message = """ plot_bfiq_range_time.py [-h] bfiq_file --beams --max-power --min-power --start-sample --end-sample
     
     Pass in the name of the bfiq array-restructured file that you want 
     to plot range time data from. All beams and arrays will be plotted 
@@ -36,11 +36,11 @@ def usage_msg():
 def plot_parser():
     parser = argparse.ArgumentParser(usage=usage_msg())
     parser.add_argument("bfiq_file", help="Name of the file to plot.")
-    parser.add_argument("--beams", help="Beams to plot.")
-    parser.add_argument("--max-power", help="Maximum Power of color scale (dB).", default=50.0)
-    parser.add_argument("--min-power", help="Minimum Power of color scale (dB).", default=10.0)
-    parser.add_argument("--start-sample", help="Sample Number to start at.", default=0)
-    parser.add_argument("--end-sample", help="Sample Number to end at.", default=80)
+    parser.add_argument("--beams", help="Beams to plot. Format as --beams=0,1,2,3,4", type=str)
+    parser.add_argument("--max-power", help="Maximum Power of color scale (dB).", default=50.0, type=float)
+    parser.add_argument("--min-power", help="Minimum Power of color scale (dB).", default=10.0, type=float)
+    parser.add_argument("--start-sample", help="Sample Number to start at.", default=0, type=int)
+    parser.add_argument("--end-sample", help="Sample Number to end at.", default=80, type=int)
     return parser
 
 
@@ -49,6 +49,11 @@ if __name__ == '__main__':
     args = args_parser.parse_args()
 
     filename = args.bfiq_file
-    
-    plot_arrays_range_time(filename, beam_nums=args.beams, num_processes=3, vmax=args.max_power, vmin=args.min_power,
+
+    if args.beams is not None:
+        beams = args.beams.split(",")
+    else:
+        beams = None
+
+    plot_arrays_range_time(filename, beam_nums=beams, num_processes=3, vmax=args.max_power, vmin=args.min_power,
                            start_sample=args.start_sample, end_sample=args.end_sample)  # plot all beams
