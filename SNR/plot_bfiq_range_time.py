2a805f38b84ce1d8fb584bee66b5dd8bafb76916
RemingtonRohel <rar129@usask.ca>
2021-12-02 12:11:24 -0600

    Added more options to argparser for plotting bfiqs.

--- SNR/plot_bfiq_range_time.py
+++ SNR/plot_bfiq_range_time.py
@@ -1,5 +1,6 @@
 # Copyright 2019 SuperDARN Canada, University of Saskatchewan
 # Author: Marci Detwiller
+# Modified: Dec. 2, 2021 (Remington Rohel)
 
 """
 This script is used to plot all beams range-time data from a single
@@ -7,12 +8,8 @@ bfiq file.
 """
 
 import argparse
-import copy
-import glob
-import os
-import sys
-
 from hdf5_rtplot_utils import plot_arrays_range_time
+
 
 def usage_msg():
     """
@@ -39,13 +36,19 @@ def usage_msg():
 def plot_parser():
     parser = argparse.ArgumentParser(usage=usage_msg())
     parser.add_argument("bfiq_file", help="Name of the file to plot.")
+    parser.add_argument("--beams", help="Beams to plot.")
+    parser.add_argument("--max-power", help="Maximum Power of color scale (dB).", default=50.0)
+    parser.add_argument("--min-power", help="Minimum Power of color scale (dB).", default=10.0)
+    parser.add_argument("--start-sample", help="Sample Number to start at.", default=0)
+    parser.add_argument("--end-sample", help="Sample Number to end at.", default=80)
     return parser
 
 
 if __name__ == '__main__':
-    parser = plot_parser()
-    args = parser.parse_args()
+    args_parser = plot_parser()
+    args = args_parser.parse_args()
 
     filename = args.bfiq_file
     
-    plot_arrays_range_time(filename) # plot all beams
+    plot_arrays_range_time(filename, beam_nums=args.beams, num_processes=3, vmax=args.max_power, vmin=args.min_power,
+                           start_sample=args.start_sample, end_sample=args.end_sample)  # plot all beams
