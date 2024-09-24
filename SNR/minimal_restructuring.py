26d77eb5c8b6c2c71374ce4dbc7a7996a1abf658
Remington Rohel <rar129@usask.ca>
2024-09-24 20:55:22 +0000

    Replacing deepdish I/O with h5py

--- SNR/minimal_restructuring.py
+++ SNR/minimal_restructuring.py
@@ -7,10 +7,8 @@ for the express purpose of plotting with hdf5_rtplot_utils.py.
 Functions
 ---------
 """
-import copy
+import h5py
 import numpy as np
-import os
-import deepdish as dd
 
 
 def antennas_iq_site_to_array(antennas_iq_file, antenna_nums=None):
@@ -37,48 +35,48 @@ def antennas_iq_site_to_array(antennas_iq_file, antenna_nums=None):
     # Dictionary of fields that will be returned
     arrays = {}
 
-    group = dd.io.load(antennas_iq_file)
-    timestamps = sorted(list(group.keys()))
+    with h5py.File(antennas_iq_file, 'r') as f:
+        timestamps = sorted(list(f.keys()))
 
-    antenna_arrays_order = group[timestamps[0]]['antenna_arrays_order']
-    arrays['antenna_arrays_order'] = antenna_arrays_order
+        antenna_arrays_order = f[timestamps[0]]['antenna_arrays_order'][:]
+        arrays['antenna_arrays_order'] = antenna_arrays_order
 
-    num_records = len(timestamps)
-    num_sequences_array = np.zeros(num_records, dtype=np.uint32)
+        num_records = len(timestamps)
+        num_sequences_array = np.zeros(num_records, dtype=np.uint32)
 
-    # typically, antenna names and antenna indices are the same except
-    # where certain antennas were skipped in data writing for any reason.
-    if antenna_nums is None or len(antenna_nums) == 0:
-        antenna_indices = list(range(0, antenna_arrays_order.size))
-        antenna_names = list(antenna_arrays_order)
-    else:
-        antenna_indices = [list(antenna_arrays_order).index('antenna_' + str(i)) for i in antenna_nums]
-        antenna_names = [f'antenna_{a}' for a in antenna_nums]
+        # typically, antenna names and antenna indices are the same except
+        # where certain antennas were skipped in data writing for any reason.
+        if antenna_nums is None or len(antenna_nums) == 0:
+            antenna_indices = list(range(0, antenna_arrays_order.size))
+            antenna_names = list(antenna_arrays_order)
+        else:
+            antenna_indices = [list(antenna_arrays_order).index(bytes(f'antenna_{i}', 'utf-8')) for i in antenna_nums]
+            antenna_names = [f'antenna_{a}' for a in antenna_nums]
 
-    # Get the maximum number of sequences for a record in the file
-    max_sequences = 0
-    for i, timestamp in enumerate(timestamps):
-        record = group[timestamp]
-        _, num_seqs, _ = record['data_dimensions']
-        num_sequences_array[i] = num_seqs
-        if num_seqs > max_sequences:
-            max_sequences = num_seqs
+        # Get the maximum number of sequences for a record in the file
+        max_sequences = 0
+        for i, timestamp in enumerate(timestamps):
+            record = f[timestamp]
+            _, num_seqs, _ = record['data_dimensions'][:]
+            num_sequences_array[i] = num_seqs
+            if num_seqs > max_sequences:
+                max_sequences = num_seqs
 
-    # We will need these dimensions for pre-allocating arrays
-    data_dimensions = group[timestamps[0]]['data_dimensions']
+        # We will need these dimensions for pre-allocating arrays
+        data_dimensions = f[timestamps[0]]['data_dimensions'][:]
 
-    # Create arrays for each of the needed fields
-    timestamps_data = np.zeros((num_records, max_sequences), dtype=np.float64)
-    data_array = np.zeros((num_records, len(antenna_indices), max_sequences, data_dimensions[2]), dtype=np.complex64)
+        # Create arrays for each of the needed fields
+        timestamps_data = np.zeros((num_records, max_sequences), dtype=np.float64)
+        data_array = np.zeros((num_records, len(antenna_indices), max_sequences, data_dimensions[2]), dtype=np.complex64)
 
-    # Copy the relevant data into the arrays
-    for i, timestamp in enumerate(timestamps):
-        record = group[timestamp]
-        num_antennas, num_seqs, num_samps = record['data_dimensions']
-        timestamps_data[i, :num_seqs] = record['sqn_timestamps']
-        data = record['data'].reshape(record['data_dimensions'])
-        data_array[i, :, :num_seqs, :] = \
-            data[antenna_indices, ...]
+        # Copy the relevant data into the arrays
+        for i, timestamp in enumerate(timestamps):
+            record = f[timestamp]
+            num_antennas, num_seqs, num_samps = record['data_dimensions'][:]
+            timestamps_data[i, :num_seqs] = record['sqn_timestamps'][:]
+            data = record['data'][:].reshape((num_antennas, num_seqs, num_samps))
+            data_array[i, :, :num_seqs, :] = \
+                data[antenna_indices, ...]
 
     # Copy to dictionary and return
     arrays['data'] = data_array
